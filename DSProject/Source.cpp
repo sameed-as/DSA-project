af2df5c29b3f28014d01fa6d5badf4122f424ba9
Awais100k <demha7159@gmail.com>
2023-12-06 09:35:46 +0500

    insert

--- DSProject/Source.cpp
+++ DSProject/Source.cpp
@@ -4,10 +4,26 @@
 #include "circularLinkedlist.h"
 #include "finger Table.h"
 #include "doublyLL.h"
+//#include "bigInt"
+#include <cmath>
 
 using namespace std;
 int main(){
 	cout << "Hello world" << endl;
+	BTree<int> b(3);
+	b.insert(1);
+	b.insert(2);
+	b.insert(3);
+	b.insert(4);
+	b.insert(5);
+	b.insert(6);
+	b.insert(7);
+	b.insert(8);
+	b.insert(9);
+	b.insert(10);
+	b.insert(11);
+
+
 	CircularLinkedList<int> cll;
 	cll.insert(10);
 	cll.insert(20);
