c86b5be74bd89a8d4a21824b9f750393a3bdf5f4
Awais Ahmed Ahmed i221225 <i221225@nu.edu.pk>
2023-12-14 19:04:05 +0500

    BTree deletion

--- DSProject/BTree.h
+++ DSProject/BTree.h
@@ -2,6 +2,7 @@
 // using the input of max nodes from user
 #pragma once
 #include<cmath>
+#include "doublyLL.h"
 #include <queue>
 #include <iostream>
 
@@ -10,6 +11,7 @@ using namespace std;
 class BNode {
 public:
     int* keys;
+    DoublyLinkedList<string>* listData;
     int t;
     int tmin;
     BNode** C;
@@ -27,25 +29,30 @@ public:
     BNode(int t1, bool leaf1) {
         t = t1;
         leaf = leaf1;
- 
+
         tmin = ceil((t + 1) / 2.0);
         keys = new int[t];
-        for (int i = 0; i < t; i++)
+        listData = new DoublyLinkedList<string>[t];
+        for (int i = 0; i < t; i++) {
             keys[i] = INT16_MAX;
+        }
         C = new BNode * [t + 1];
         for (int i = 0;i < t + 1;i++) {
             C[i] = nullptr;
         }
 
         n = 0;
+
     }
     void splitChild(int i, BNode* y) {
         BNode* z = new BNode(y->t, y->isleaf());
         z->n = tmin - 1;
 
         for (int j = 0; j < z->n; j++) {
-            if (j + tmin < t)
+            if (j + tmin < t) {
                 z->keys[j] = y->keys[j + tmin];
+                z->listData[j] = y->listData[j + tmin];
+            }
             // z->n++;
             else
                 z->n--;
@@ -62,22 +69,39 @@ public:
 
         C[i + 1] = z;
 
-        for (int j = n - 1; j >= i; j--)
+        for (int j = n - 1; j >= i; j--) {
             keys[j + 1] = keys[j];
+            listData[j + 1] = listData[j];
+        }
 
         keys[i] = y->keys[tmin - 1];
+        listData[i] = y->listData[tmin - 1];
         n = n + 1;
+        for (int i = n + 1; i < t + 1;i++) {
+            C[i] = nullptr;
+        }
+        for (int i = z->n + 1; i < t + 1; i++) {
+            z->C[i] = nullptr;
+        }
+        for (int i = y->n + 1;i < t + 1;i++) {
+            y->C[i] = nullptr;
+        }
     }
-    void insertNonFull(int k) {
+    void insertNonFull(int k, string dataVal) {
         int i = n - 1;
 
         if (isleaf() == true) {
             while (i >= 0 && keys[i] > k) {
                 keys[i + 1] = keys[i];
+                listData[i + 1] = listData[i];
                 i--;
             }
 
             keys[i + 1] = k;
+            if (listData[i + 1].isEmpty())
+                listData[i + 1].insert(dataVal);
+            else
+                listData[i + 1].getHead()->data = dataVal;
             n = n + 1;
         }
         else {
@@ -90,9 +114,10 @@ public:
                 if (keys[i + 1] < k)
                     i++;
             }
-            C[i + 1]->insertNonFull(k);
+            C[i + 1]->insertNonFull(k, dataVal);
         }
     }
+
     void traverse();
 
 };
@@ -103,8 +128,7 @@ public:
     int t;
     int tmin;
 
-    BTree()
-    {
+    BTree() {
         root = nullptr;
         t = tmin = 0;
     }
@@ -115,9 +139,8 @@ public:
         tmin = ceil((t + 1) / 2.0);
     }
 
-    void helperTraverse(BNode*& root , queue<BNode*>& que) {
-        for (int i = 0; i <= root->n; i++)
-        {
+    void helperTraverse(BNode*& root, queue<BNode*>& que) {
+        for (int i = 0; i <= root->n; i++) {
             if (root->C[i]) {
                 helperTraverse(root->C[i], que);
                 que.push(root->C[i]);
@@ -133,41 +156,280 @@ public:
     void traverse() {
         if (root != NULL) {
             queue <BNode*> que;
-            helperTraverse(root , que);
+            helperTraverse(root, que);
         }
     }
 
-    void insert(int k) {
+    void insert(int k, string dataValue) {
         if (root == NULL) {
             root = new BNode(t, true);
             root->keys[0] = k;
+            root->listData[0].insert(dataValue);
             root->n = 1;
         }
         else {
-            if (root->n == t) {
-                BNode* s = new BNode(t, false);
+            if (!Search(root, k, dataValue)) {
+                if (root->n == t) {
+                    BNode* s = new BNode(t, false);
 
-                s->C[0] = root;
+                    s->C[0] = root;
 
-                s->splitChild(0, root);
+                    s->splitChild(0, root);
 
-                int i = 0;
-                if (s->keys[0] < k)
-                    i++;
-                s->C[i]->insertNonFull(k);
+                    int i = 0;
+                    if (s->keys[0] < k)
+                        i++;
+                    s->C[i]->insertNonFull(k, dataValue);
 
-                root = s;
+                    root = s;
+                }
+                else
+                    root->insertNonFull(k, dataValue);
             }
-            else
-                root->insertNonFull(k);
         }
+
+    }
+    bool Search(BNode*& r, int k, string dataVal) {
+        int i = 0;
+        while (i < r->n && k > r->keys[i]) {
+            i++;
+        }
+        if (i < r->n && k == r->keys[i]) {
+            r->listData[i].insert(dataVal);
+            return true;
+        }
+        if (r->isleaf()) {
+            return false;
+        }
+        return Search(r->C[i], k, dataVal);
+    }
+    bool  Search_forDel(BNode*& r, int k) {
+        int i = 0;
+        while (i < r->n && k > r->keys[i]) {
+            i++;
+        }
+        if (i < r->n && k == r->keys[i]) {
+            if (r->listData[i].size == 1) {
+                return false;
+            }
+            else {
+                int choice = 0;
+                r->listData[i].print();
+                cout << "Choose your Data to delete: " << endl;
+                cin >> choice;
+                r->listData[i].deleteData(choice);
+            }
+            return true;
+        }
+        if (r->isleaf()) {
+            return false;
+        }
+        return Search_forDel(r->C[i], k);
+    }
+    void delete_helper(BNode*& node, BNode*& parent, int& k, int& pos, bool& found, bool& k_swaped, bool& delete_occured, BNode*& root, bool& root_Updated) {
+        int i = 0;
+        bool vapis_mur = true;
+        while (i < node->n && k > node->keys[i]) {
+            i++;
+        }
+        if (node->keys[i] == k && i < node->n)
+            found = true;
+        if (node->isleaf() && !found) {
+            cout << "Key doesnt exist " << endl;
+            return;
+        }
+        if (!found) {
+            delete_helper(node->C[i], node, k, i, found, k_swaped, delete_occured, root, root_Updated);
+        }
+        if (!node->isleaf() && !k_swaped && !delete_occured) {
+            BNode* change_k = node;
+            node = node->C[i];//go left
+            while (!node->isleaf()) {//and then right
+                node = node->C[node->n];
+            }
+            //the value to be deleted is replaced with the largest value in left subtree
+            int temp = change_k->keys[i];
+            change_k->keys[i] = node->keys[node->n - 1];
+            node->keys[node->n - 1] = temp;
+            found = false;
+            k_swaped = true;
+            vapis_mur = false;
+            delete_helper(change_k->C[i], change_k, k, i, found, k_swaped, delete_occured, root, root_Updated);//again deletion will be call for updated key
+            node = change_k;
+        }
+        if (node == root && root->n == 1 && root->isleaf()) {
+            root->n--;
+            root = NULL;
+            return;
+        }
+        if (!root_Updated)
+            if ((vapis_mur && node->isleaf()) || (node->n < tmin - 1 && parent != nullptr)) {
+                if (node->n >= tmin) {
+                    while (i < node->n) {
+                        node->keys[i] = node->keys[i + 1];
+                        i++;
+                    }
+                    node->n -= 1;
+                    delete_occured = true;
+                }
+                else {
+                    //Left sibling case
+                    if (pos > 0 && parent->C[pos - 1]->n >= tmin) {
+                        int idx = i;
+                        while (idx > 0) {
+                            node->keys[idx] = node->keys[idx - 1];
+                            idx--;
+                        }
+
+                        node->keys[0] = parent->keys[pos - 1];
+                        parent->keys[pos - 1] = parent->C[pos - 1]->keys[parent->C[pos - 1]->n - 1];
+                        parent->C[pos - 1]->n--;
+                        idx = node->n;
+                        while (idx > 0) {
+                            node->C[idx] = node->C[idx - 1];
+                            idx--;
+                        }
+                        node->C[0] = parent->C[pos - 1]->C[parent->C[pos - 1]->n + 1];
+                        parent->C[pos - 1]->C[parent->C[pos - 1]->n + 1] = nullptr;
+                        delete_occured = true;
+                    }
+                    //RightSibling Case
+                    else if (pos <= parent->n - 1 && parent->C[pos + 1]->n >= tmin) {
+                        int idx = i;
+                        while (idx < node->n - 1) {
+                            node->keys[idx] = node->keys[idx + 1];
+                            idx++;
+                        }
+                        if (node->n == 0)
+                            node->n++;
+                        node->keys[node->n - 1] = parent->keys[pos];
+                        parent->keys[pos] = parent->C[pos + 1]->keys[0];
+                        int j = 0;
+                        while (j < parent->C[pos + 1]->n - 1) {
+                            parent->C[pos + 1]->keys[j] = parent->C[pos + 1]->keys[j + 1];
+                            j++;
+                        }
+                        parent->C[pos + 1]->n--;
+                        node->C[node->n] = parent->C[pos + 1]->C[0];
+                        idx = 0;
+                        while (idx < parent->C[pos + 1]->n + 1) {
+                            parent->C[pos + 1]->C[idx] = parent->C[pos + 1]->C[idx + 1];
+                            idx++;
+                        }
+                        delete_occured = true;
+                    }
+                    //Merge Case
+                    else {
+                        //Case left siblng merge
+                        if (pos > 0) {
+                            int idx = i;
+                            if (!delete_occured) {
+                                while (idx < node->n - 1) {
+                                    node->keys[idx] = node->keys[idx + 1];
+                                    idx++;
+                                }
+                                node->n--;
+                            }
+                            parent->C[pos - 1]->keys[parent->C[pos - 1]->n] = parent->keys[pos - 1];
+                            parent->C[pos - 1]->n++;
+                            int inc_n = 0;
+                            idx = 0;
+                            while (idx < node->n) {
+                                parent->C[pos - 1]->keys[idx + parent->C[pos - 1]->n] = node->keys[idx];
+                                inc_n++;
+                                idx++;
+                            }
+                            idx = 0;
+                            while (idx < node->n + 1) {
+                                parent->C[pos - 1]->C[idx + parent->C[pos - 1]->n] = node->C[idx];/////////////////////////////////
+                                idx++;
+                            }
+                            parent->C[pos - 1]->n += inc_n;
+                            idx = pos - 1;
+                            while (idx < parent->n - 1) {
+                                parent->keys[idx] = parent->keys[idx + 1];
+                                idx++;
+                            }
+                            idx = pos;
+                            while (idx < parent->n + 1) {
+                                parent->C[idx] = parent->C[idx + 1];
+                                idx++;
+                            }
+                            parent->n--;
+                            if (root->n == 0) {
+                                root = node;
+                                root_Updated = true;
+                            }
+                            delete_occured = true;
+                        }
+                        //case right sibling merge
+                        else {
+                            int idx = i;
+                            int inc_n = 0;
+                            if (!delete_occured) {
+                                while (idx < node->n - 1) {
+                                    node->keys[idx] = node->keys[idx + 1];
+                                    idx++;
+                                }
+                                node->keys[node->n - 1] = parent->keys[pos];
+
+                            }
+                            else {
+                                node->keys[node->n] = parent->keys[pos];
+                                node->n++;
+                            }
+                            idx = 0;
+                            while (idx < parent->C[pos + 1]->n) {
+                                node->keys[node->n + idx] = parent->C[pos + 1]->keys[idx];
+                                idx++;
+                                inc_n++;
+                            }
+                            idx = 0;
+                            while (idx < parent->C[pos + 1]->n + 1) {
+                                node->C[node->n + idx] = parent->C[pos + 1]->C[idx];///////////////////////////////////////
+                                idx++;
+                            }
+                            idx--;
+                            node->n += idx;
+                            idx = pos;
+                            while (idx < parent->n - 1) {
+                                parent->keys[idx] = parent->keys[idx + 1];
+                                idx++;
+                            }
+                            idx = pos + 1;
+                            while (idx < parent->n + 1) {
+                                parent->C[idx] = parent->C[idx + 1];
+                                idx++;
+                            }
+                            parent->n--;
+                            if (root->n == 0) {
+                                root = node;
+                                root_Updated = true;
+                            }
+                            delete_occured = true;
+                        }
+                    }
+                }
+            }
+
+    }
+
+    void deletion(int k) {
+        bool found = false;
+        bool k_swaped = false;
+        bool delete_occured = false;
+        bool root_Updated = false;
+        BNode* temp = nullptr;
+        int pos = 0;
+        if (!Search_forDel(root, k))
+            delete_helper(root, temp, k, pos, found, k_swaped, delete_occured, root, root_Updated);
     }
 };
 
 // Traverse the nodes
 
 void BNode::traverse() {
-    
+
     int i = 0;
     if (C[i] != nullptr) {
         C[i]->traverse();
@@ -175,7 +437,7 @@ void BNode::traverse() {
 
     for (i = 1; i <= n; i++) {
         cout << " " << keys[i - 1];
-        
+
         if (C[i] != nullptr)
             C[i]->traverse();
     }
